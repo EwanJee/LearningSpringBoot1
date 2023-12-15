061d24890e29a585327f1edd162b4265f3eb7b15
Ewan Jee <830jiforcareer@gmail.com>
2023-12-15 17:36:17 -0500

    completed springboot2 and init springbootJPA

--- springboot2/src/main/java/com/building/springboot/CourseController.java
+++ springboot2/src/main/java/com/building/springboot/CourseController.java
@@ -1,4 +1,23 @@
 package com.building.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class CourseController {
+
+    // courses
+    // Course : id, name, author
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(0,"Learn AWS","SpringBoot"),
+                new Course(1,"Learn DevOps","SpringBoot"),
+                new Course(2,"Learn Azure","SpringBoot")
+        );
+    }
 }
