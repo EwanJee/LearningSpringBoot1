6a2f828e6e3c628255049cbcb26668fe56a63174
Ewan Jee <830jiforcareer@gmail.com>
2024-01-02 23:41:55 +0900

    Learned Swagger-ui and internationalization

--- webservices/src/main/java/com/learning/webservices/helloworld/HelloWorldController.java
+++ webservices/src/main/java/com/learning/webservices/helloworld/HelloWorldController.java
@@ -1,15 +1,41 @@
 package com.learning.webservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //REST API
 @RestController
 public class HelloWorldController {
 
+    private final MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // hello-world
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+
+    /**
+     * good.morning.message
+     *
+     * `en` - English (Good Morning)
+     * `fr` - French (Bonjour)
+     */
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null, "Default",locale);
+
     }
 }
