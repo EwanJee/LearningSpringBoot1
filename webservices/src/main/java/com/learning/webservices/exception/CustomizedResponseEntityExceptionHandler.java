dcfc354a4ca7ceb395e4be073d1ff129db2c5d4c
Ewan Jee <830jiforcareer@gmail.com>
2024-01-04 14:45:36 +0900

    Learned Versioning and JsonIgnore

--- webservices/src/main/java/com/learning/webservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ webservices/src/main/java/com/learning/webservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -18,7 +18,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),ex.getMessage(),request.getDescription(false));
-        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
--- webservices/src/main/java/com/learning/webservices/user/UserResource.java
+++ webservices/src/main/java/com/learning/webservices/user/UserResource.java
@@ -2,6 +2,11 @@ package com.learning.webservices.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,13 +26,24 @@ public class UserResource {
     public List<User> retrieveUserList(){
         return userDaoService.findAll();
     }
-    @GetMapping("/users/{id}")
+    /*@GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
         User user = userDaoService.findById(id);
         if(user == null){
             throw new UserNotFoundException("id:" + id);
         }
         return user;
+    }*/
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("id:" + id);
+        }
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveUserList());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id){
@@ -40,5 +56,13 @@ public class UserResource {
                 .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         // -> location header
         return ResponseEntity.created(location).build();
+    }
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("HeaderName", "HeaderValue");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body("Hello World!");
     }
 }
